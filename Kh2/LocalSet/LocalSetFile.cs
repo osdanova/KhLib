92c0266bf957911ce38c12aa98f3b6ff227e38f4
osdanova <osdanovaonline@outlook.com>
2025-03-03 12:08:28 +0100

    LocalSet file

---
+++ Kh2/LocalSet/LocalSetFile.cs
@@ -0,0 +1,41 @@
+﻿namespace KhLib.Kh2.LocalSet
+{
+    public class LocalSetFile
+    {
+        private const int FINAL_PADDING = 400;
+
+        public List<LocalSetWorld> Files = new List<LocalSetWorld>();
+
+        public static LocalSetFile Read(byte[] byteFile)
+        {
+            LocalSetFile thisFile = new LocalSetFile();
+
+            BinaryArchive archive = BinaryArchive.Read(byteFile);
+
+            foreach(var a in archive.Entries)
+            {
+                thisFile.Files.Add(LocalSetWorld.Read(a.File));
+            }
+
+            return thisFile;
+        }
+
+        public byte[] GetAsByteArray()
+        {
+            BinaryArchive bar = new BinaryArchive();
+
+            foreach (LocalSetWorld world in Files)
+            {
+                BinaryArchive.Entry entry = new BinaryArchive.Entry();
+                entry.Name = "loca";
+                entry.Type = BinaryArchive.EntryType.List;
+                entry.File = world.GetAsByteArray();
+                bar.Entries.Add(entry);
+            }
+
+            byte[] byteFile = bar.GetAsByteArray();
+            Array.Resize(ref byteFile, byteFile.Length + FINAL_PADDING);
+            return byteFile;
+        }
+    }
+}
---
+++ Kh2/LocalSet/LocalSetWorld.cs
@@ -0,0 +1,25 @@
+﻿using KhLib.Kh2.Structs;
+using Xe.BinaryMapper;
+
+namespace KhLib.Kh2.LocalSet
+{
+    public class LocalSetWorld : BaseTableFile<LocalSetWorld.Entry>
+    {
+        public LocalSetWorld() : base(4, 4) { }
+
+        public static LocalSetWorld Read(byte[] byteFile)
+        {
+            LocalSetWorld file = new LocalSetWorld();
+            file.ReadFile(byteFile);
+
+            return file;
+        }
+
+        // LOCALSET
+        public class Entry
+        {
+            [Data] public short LocalSetId { get; set; }
+            [Data] public short AreaId { get; set; }
+        }
+    }
+}
